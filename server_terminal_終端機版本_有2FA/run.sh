6547adb4c776aa4682358bbc5b7766f50cbf0b6e
Yuchen-ck <2589877yen@gmail.com>
2022-12-15 12:52:33 +0800

    dockerfile_start

--- server_terminal_終端機版本_有2FA/run.sh
+++ server_terminal_終端機版本_有2FA/run.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm -it     -v /tmp/.X11-unix:/tmp/.X11-unix     -e DISPLAY=$DISPLAY     -u qtuser     server/system:latest python3.7 /home/qtuser/Server-system/system_main.py
+sudo docker run --rm -it  -u qtuser     server/system:latest python3.7 /home/qtuser/Server-system/main.py
